8ec8c4db8db3e3c98e8820ea76bc6dda6523c02e
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-07 15:27:54 +0200

    Create generic crud service interface

---
+++ src/Pin.Spoticlone.Web/Interfaces/ICRUDService.cs
@@ -0,0 +1,13 @@
+﻿using Pin.Spoticlone.Web.Models;
+
+namespace Pin.Spoticlone.Web.Interfaces
+{
+    public interface ICRUDService<T> where T : BaseEntitie
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetById(int id);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(Guid id);
+    }
+}
