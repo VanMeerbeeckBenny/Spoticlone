75e472d547866ed37c7feb9efc980a7dcbf22bc2
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-07 15:28:05 +0200

    Create artist entitie

---
+++ src/Pin.Spoticlone.Web/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿namespace Pin.Spoticlone.Web.Models
+{
+    public class Artist : BaseEntitie
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }      
+        public string SpotifyId { get; set; }
+        public Uri Image { get; set; }
+    }
+}
