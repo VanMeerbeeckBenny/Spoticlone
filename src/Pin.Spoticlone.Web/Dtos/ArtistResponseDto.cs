e52df6e8b0905afcf027f40961ed77e42dfab810
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-07 15:26:57 +0200

    Create artist dto

---
+++ src/Pin.Spoticlone.Web/Dtos/ArtistResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using Pin.Spoticlone.Core.Dtos;
+using Pin.Spoticlone.Core.Dtos.Base;
+
+namespace Pin.Spoticlone.Web.Dtos
+{
+    public class ArtistResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }
+        public string SpotifyId { get; set; }
+        public Uri Image { get; set; }
+        public ICollection<GenreResponseDto> Genres { get; set; }
+        public int AlbumsCount { get; set; }
+    }
+}
