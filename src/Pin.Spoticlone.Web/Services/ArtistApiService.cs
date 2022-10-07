39717d3b503b73b352f58cd6c501371f2658a5a0
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-07 15:28:29 +0200

    Create artist api service

---
+++ src/Pin.Spoticlone.Web/Services/ArtistApiService.cs
@@ -0,0 +1,50 @@
+﻿using Pin.Spoticlone.Web.Dtos;
+using Pin.Spoticlone.Web.Interfaces;
+using Pin.Spoticlone.Web.Models;
+
+namespace Pin.Spoticlone.Web.Services
+{
+    public class ArtistApiService : ICRUDService<Artist>
+    {
+        HttpClient _httpClient = null;
+
+
+        public ArtistApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public Task Create(Artist item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Artist>> GetAllAsync()
+        {
+            var artists = await _httpClient.GetFromJsonAsync<IEnumerable<ArtistResponseDto>>("https://localhost:44319/api/Artists");
+            return artists.Select(a => new Artist
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Followers = a.Followers,
+                Popularity = a.Popularity,
+                Image = a.Image,
+            });
+        }
+
+        public Task<Artist> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Artist item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
