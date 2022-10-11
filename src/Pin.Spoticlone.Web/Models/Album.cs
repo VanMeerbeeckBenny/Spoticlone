7432b350bffab2ecd0f55199cbb2b1ab8d1d095f
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-11 15:44:22 +0200

    Create enitities

---
+++ src/Pin.Spoticlone.Web/Models/Album.cs
@@ -0,0 +1,10 @@
+﻿namespace Pin.Spoticlone.Web.Models
+{
+    public class Album:BaseEntitie
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Uri Image { get; set; }
+
+    }
+}
---
+++ src/Pin.Spoticlone.Web/Models/Genre.cs
@@ -0,0 +1,7 @@
+﻿namespace Pin.Spoticlone.Web.Models
+{
+    public class Genre : BaseEntitie
+    {
+        public string Name { get; set; }
+    }
+}
