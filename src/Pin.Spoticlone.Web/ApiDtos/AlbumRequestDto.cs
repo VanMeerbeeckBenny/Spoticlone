148bc2031ea930c4be085f07133877c5f165eed6
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-11 15:43:23 +0200

    Create album dto

---
+++ src/Pin.Spoticlone.Web/ApiDtos/AlbumRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Pin.Spoticlone.Core.ApiDtos;
+using System;
+
+namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class AlbumRequestDto : DtoBase
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Uri Image { get; set; }
+        public Guid ArtistId { get; set; }
+    }
+}
