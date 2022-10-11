2fe3c2cbe47380fa91daac1cf82b8d028c9e2881
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-11 12:29:01 +0200

    Change map name

---
+++ src/Pin.Spoticlone.Web/ApiDtos/ArtistResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using Pin.Spoticlone.Core.ApiDtos;
+
+namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class ArtistResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }
+        public string SpotifyId { get; set; }
+        public Uri Image { get; set; }
+        public ICollection<GenreResponseDto> Genres { get; set; }
+        public int AlbumsCount { get; set; }
+    }
+}
---
+++ src/Pin.Spoticlone.Web/ApiDtos/DtoBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Pin.Spoticlone.Core.ApiDtos
+{
+    public abstract class DtoBase
+    {
+        public Guid Id { get; set; }
+    }
+}
