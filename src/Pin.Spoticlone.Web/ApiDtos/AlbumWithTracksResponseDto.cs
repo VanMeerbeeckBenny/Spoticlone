41ff42100866a76f2dd0bce4ba7cd88e48c2eff5
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-12 20:24:08 +0200

    Create dto for ablum with track

---
+++ src/Pin.Spoticlone.Web/ApiDtos/AlbumWithTracksResponseDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class AlbumWithTracksResponseDto
+    {
+        public AlbumResponseDto Album { get; set; }
+        public IEnumerable<TrackResponseDto> Tracks { get; set; }
+    }
+}
---
+++ src/Pin.Spoticlone.Web/ApiDtos/TrackResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class TrackResponseDto : DtoBase
+    {
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public bool Explicit { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public string AlbumName { get; set; }
+        public Guid AlbumId { get; set; }
+    }
+}
