bfe25369bfa34448f111e51eb48d7373d1a67b04
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-12 14:18:51 +0200

    Create artistwithalbums response dto

---
+++ src/Pin.Spoticlone.Web/ApiDtos/ArtistWithAlbumsResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class ArtistWithAlbumsResponseDto
+    {
+        public ArtistResponseDto Artist { get; set; }
+        public IEnumerable<AlbumResponseDto> Albums { get; set; }
+    }
+}
