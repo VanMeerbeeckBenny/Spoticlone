775a46f43333db108708952002feb11a8e5534d8
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-12 20:24:44 +0200

    Create track model

---
+++ src/Pin.Spoticlone.Web/Models/Track.cs
@@ -0,0 +1,12 @@
+﻿namespace Pin.Spoticlone.Web.Models
+{
+    public class Track:BaseEntitie
+    {
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public bool Explicit { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }    
+        public Guid AlbumId { get; set; }
+    }
+}
