4e8586c4fd17dc5a0c8d55deaaf90729e7c24eb0
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-13 01:19:59 +0200

    Add data anotation

--- src/Pin.Spoticlone.Web/Models/Track.cs
+++ src/Pin.Spoticlone.Web/Models/Track.cs
@@ -1,9 +1,14 @@
-﻿namespace Pin.Spoticlone.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pin.Spoticlone.Web.Models
 {
     public class Track:BaseEntitie
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Duration { get; set; }
+        [Required]
         public bool Explicit { get; set; }
         public int TrackNumber { get; set; }
         public int DiscNumber { get; set; }    
