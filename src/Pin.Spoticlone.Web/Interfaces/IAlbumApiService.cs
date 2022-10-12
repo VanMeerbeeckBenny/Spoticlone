8e4de46feb31b3686f71201be6e80e82067f8610
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-12 20:25:31 +0200

    Add get album with track by id

--- src/Pin.Spoticlone.Web/Interfaces/IAlbumApiService.cs
+++ src/Pin.Spoticlone.Web/Interfaces/IAlbumApiService.cs
@@ -1,9 +1,11 @@
 ﻿using Pin.Spoticlone.Web.Models;
+using System.Threading.Tasks;
 
 namespace Pin.Spoticlone.Web.Interfaces
 {
     public interface IAlbumApiService
     {
         Task<ItemResultModel<Album>> GetAlbumsByArtistIdAsync(Guid artistId);
+        Task<ItemResultModel<AlbumWithTracks>> GetAlbumsWithTracksByIdAsync(Guid artistId);
     }
 }
