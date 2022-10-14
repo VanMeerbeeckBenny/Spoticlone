77d1cb2ccbcfba004cb754126c7b2d23468dd82c
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-14 18:28:04 +0200

    Create track request dto

---
+++ src/Pin.Spoticlone.Web/ApiDtos/TrackRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Pin.Spoticlone.Web.ApiDtos
+{
+    public class TrackRequestDto : DtoBase
+    {
+        public string Name { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public int DurationMs { get; set; }
+        public bool Explicit { get; set; }
+        public Guid AlbumId { get; set; }
+    }
+}
