e60fb5fe5706c8c9a3182ad4490b6f8815c6d780
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-14 17:38:23 +0200

    Add data annotation

--- src/Pin.Spoticlone.Web/Models/Track.cs
+++ src/Pin.Spoticlone.Web/Models/Track.cs
@@ -5,12 +5,17 @@ namespace Pin.Spoticlone.Web.Models
     public class Track:BaseEntitie
     {
         [Required]
+        [MaxLength(100,ErrorMessage ="{0} can not be longer then {1}")]
+        [MinLength(2, ErrorMessage = "{0} can not be longer then {1}")]
         public string Title { get; set; }
         [Required]
-        public string Duration { get; set; }
-        [Required]
+        public string Duration { get; set; }        
         public bool Explicit { get; set; }
+        [Required]
+        [Range(1,int.MaxValue)]
         public int TrackNumber { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
         public int DiscNumber { get; set; }    
         public Guid AlbumId { get; set; }
     }
