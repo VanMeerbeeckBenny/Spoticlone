57460aa6fa3fa0ca8ba3073d050564204106333c
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-14 18:26:58 +0200

    Create trackapiservice interface

---
+++ src/Pin.Spoticlone.Web/Interfaces/ITrackApiService.cs
@@ -0,0 +1,13 @@
+﻿using Pin.Spoticlone.Web.Models;
+
+namespace Pin.Spoticlone.Web.Interfaces
+{
+    public interface ITrackApiService
+    {
+        Task<ItemResultModel<Track>> GetTrackById(Guid id);
+        Task<ItemResultModel<Track>> UpdateAsync(string title, string duration, bool isExplicit, int trackNumber, int discNumber,Guid id, Guid albumId);
+        Task<ItemResultModel<Track>> AddAsync(string title, string duration, bool isExplicit, int trackNumber, int discNumber, Guid albumId);
+        Task<ItemResultModel<Track>> DeleteAsync(Guid id);
+
+    }
+}
