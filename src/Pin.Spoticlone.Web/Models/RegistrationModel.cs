f7e8b16e306a735afbd6790291fd5577d1638961
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-18 15:22:15 +0200

    Create registration model

---
+++ src/Pin.Spoticlone.Web/Models/RegistrationModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pin.Spoticlone.Web.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "")]
+        [EmailAddress(ErrorMessage = "Not a valid E-mail")]
+        public string Email { get; set; }
+        public bool IsRegistered { get; set; }
+    }
+}
