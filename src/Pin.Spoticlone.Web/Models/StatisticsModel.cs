706448db526382840662a6293fbc3f82428ded15
Benny <benny.van.meerbeeck@student.howest.be>
2022-10-18 10:38:22 +0200

    Create statistics model

---
+++ src/Pin.Spoticlone.Web/Models/StatisticsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.Spoticlone.Web.Models
+{
+    public class StatisticsModel
+    {
+        public IEnumerable<Artist> TopFollowers { get; set; }
+        public IEnumerable<Artist> TopPopularities { get; set; }
+        public IEnumerable<Album> TopAlbumDurations { get; set; }
+        public IEnumerable<Artist> TopArtistWithMostAlbums { get; set; }
+        public IEnumerable<Track> TopTrackDurations { get; set; }
+        public string TotalListeningTime { get; set; }
+    }
+}
